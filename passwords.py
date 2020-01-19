bec0d0416a75faccb8c143cf9916ee61d4c204a9
Luscsus <48566270+Luscsus@users.noreply.github.com>
2020-01-19 10:18:54 +0100

    Delete passwords.py

--- passwords.py
+++
@@ -1,6 +0,0 @@
-secret_key = "rKamaQoOurRZ2H0Kn5bSseKcBqZZvxEts/dSOubN"
-id_key = "PKTGFGWA5IM19HAKH6QC"
-alpha_vantage_key = "D0Y6Q88STILCVF3D"
-email = "luka1.grobelnik@gmail.com"
-password = "Sbsforza1"
-send_to_email = "luka1.grobelnik@gmail.com"
